84aa6f770b567fe5bb37ebda2b1d00e62d53ef1b
roxlu <info@roxlu.com>
2013-02-26 16:58:34 +0100

    paren mode fix

--- init.el
+++ init.el
@@ -12,12 +12,14 @@
 (setq ido-max-work-directory-list 100)
 (setq ido-max-work-file-list 300)
 
+(show-paren-mode 1)
+
 ; backups
-; (setq
-;    backup-by-copying t      ; don't clobber symlinks
-;    backup-directory-alist
-;     '(("." . "~/.saves"))    ; don't litter my fs tree
-;    delete-old-versions t
-;    kept-new-versions 6
-;    kept-old-versions 2
-;    version-control t)       ; use versioned backups
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
