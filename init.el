29aaa0ae2bc2c137fa941d0539ac2cb7cf135e01
roxlu <diederick@apollomedia.nl>
2015-10-23 15:15:20 +0200

    Merged

--- init.el
+++ init.el
@@ -83,7 +83,6 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
->>>>>>> origin/master
 
 ; css
 (add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
